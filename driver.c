e73642b5a591f09d3db6ec2ad552ee66c028e6c3
Kyle Thomas <mr.kyle.thomas@gmail.com>
2019-05-06 14:33:04 -0700

    fixed command line processing

--- driver.c
+++ driver.c
@@ -10,7 +10,7 @@
  */
 
 
-#include <getopt.h>
+//#include <getopt.h>
 #include <limits.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -19,59 +19,63 @@
 #include "caesar.h"
 #include "parse_file.h"
 
+#define ALPH_FLAG "-a"
+#define BOTH_FLAG "-af"
+#define BACK_FLAG "-fa"
+#define FILE_FLAG "-f"
+#define HELP_FLAG "-h"
+
 
 /* Prints out help when the user doesn't pass in the correct arguments */
-void usage(void) {
-    fprintf(stderr, "caesar shift [option] stringOrFilename\n");
-    fprintf(stderr, "\tOptions:\n\t-f\tTreat stringOrFilename as filename");
-    fprintf(stderr, "(default is string)\n");
-    fprintf(stderr, "\t-a\tOnly perform cipher on letters.\n");
-    fprintf(stderr, "\t-h\tPrint this help message.\n");
+void usage(FILE *output) {
+    fprintf(output, "caesar shift [option] stringOrFilename\n");
+    fprintf(output, "\tOptions:\n\t-f\tTreat stringOrFilename as filename");
+    fprintf(output, "(default is string)\n");
+    fprintf(output, "\t-a\tOnly perform cipher on letters.\n");
+    fprintf(output, "\t-h\tPrint this help message.\n");
+    fprintf(output, "example:\n\tcaesar 5 -af sample.txt\n\tcaesar 5 \"sample text\"\n");
+    fprintf(output, "\n");
     return;
 }
 
 
 int main(int argc, char **argv)
 {
-    if (argc == 1) {
+    if (!argv[1]) {
         fprintf(stderr, "Error: Too few arguments.\n");
-        usage();
+        usage(stderr);
         exit(EXIT_FAILURE);
-    } else if (argc > 5) {
+    }
+    if (!strcmp(argv[1], HELP_FLAG)) {
+        usage(stdout);
+        exit(EXIT_SUCCESS);
+    }
+    if (argc < 3) {
+        fprintf(stderr, "Error: Too few arguments.\n");
+        usage(stderr);
+        exit(EXIT_FAILURE);
+    } else if (argc > 4) {
         fprintf(stderr, "Error: Too many arguments.\n");
-        usage();
+        usage(stderr);
         exit(EXIT_FAILURE);
     }
 
-    int option = 0;
     int alph_flag = 0;
     int shift = atoi(argv[1]);
     char *str_to_cipher;
-    char filename[PATH_MAX];
-    filename[0] = '\0';
 
-    if (argc == 3) {  // just shift amount and string to shift
-        str_to_cipher = malloc(sizeof(char) * strlen(argv[optind]));
-        strncpy(str_to_cipher, argv[2], strlen(argv[2]));
+    if (!strcmp(argv[2], ALPH_FLAG)) {
+        alph_flag = 1;
+    } else if (!strcmp(argv[2], FILE_FLAG)) {
+        str_to_cipher = malloc(sizeof(char) * BUFF_SIZE);
+        parse_file(argv[3], &str_to_cipher);
+    } else if ( !strcmp(argv[2], BOTH_FLAG) || !strcmp(argv[2], BOTH_FLAG) ) {
+        alph_flag = 1;
+        str_to_cipher = malloc(sizeof(char) * BUFF_SIZE);
+        parse_file(argv[3], &str_to_cipher);
     } else {
-        while ((option = getopt(argc, argv, "f:ha")) != -1) {
-            switch (option) {
-
-                case 'f':
-                    strcpy(filename, optarg);
-                    str_to_cipher = malloc(sizeof(char) * BUFF_SIZE);
-                    parse_file(optarg, &str_to_cipher);
-                    break;
-
-                case 'h':
-                    usage();
-                    exit(EXIT_SUCCESS);
-
-                case 'a':
-                    alph_flag = 1;
-                    break;
-            }
-        }
+        str_to_cipher = malloc(sizeof(char) * strlen(argv[2]));
+        strncpy(str_to_cipher, argv[2], strlen(argv[2]));
     }
 
     caesar(str_to_cipher, shift, alph_flag);
